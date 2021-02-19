6a515c00102f3f0479a401664335c02b3742d35b
Dylan White <dylan.whitej@gmail.com>
2021-02-18 17:54:17 -0800

    fixed test error

--- __tests__/module.test.js
+++ __tests__/module.test.js
@@ -5,17 +5,19 @@ const { modules } = require('../data.js')
 
 it('should retrieve the module with the corresponding id', async done => {
 
-    const myData = {
-        "id": "ikarie",
-        "brand": "BASTL",
-        "moduleName": "Ikarie Stereo Filter",
-        "image": "../assets/ikarie.jpg",
-        "category": "Filter",
-        "size": 8,
-        "description": "The next collaboration between Bastl and Casper Electronics has landed, and the Ikarie Stereo Filter is ready for spacey resonances and interstellar spatial modulation. Rather than offering discrete outputs for lowpass and highpass like most state-variable filter designs, Ikarie will continuously morph between the two, and depending on how it is patched, an array of resonant bandpass or stereo notch filtering will be heard.",
-        "price": 259,
-        "inStock": false
-    }
+    const myData = [
+        {
+            "id": "ikarie",
+            "brand": "BASTL",
+            "moduleName": "Ikarie Stereo Filter",
+            "image": "ikarie.jpg",
+            "category": "Filter",
+            "size": 8,
+            "description": "The next collaboration between Bastl and Casper Electronics has landed, and the Ikarie Stereo Filter is ready for spacey resonances and interstellar spatial modulation. Rather than offering discrete outputs for lowpass and highpass like most state-variable filter designs, Ikarie will continuously morph between the two, and depending on how it is patched, an array of resonant bandpass or stereo notch filtering will be heard.",
+            "price": 259,
+            "inStock": false
+        }
+    ]
     const response = await request.get('/module/ikarie')
 
     expect(response.status).toBe(200)
@@ -30,7 +32,7 @@ it('should display only modules with instock: true', async done => {
             "id": "chord",
             "brand": "Qu-Bit",
             "moduleName": "Chord V2",
-            "image": "../assets/chord.jpg",
+            "image": "chord.jpg",
             "category": "Oscillator",
             "size": 14,
             "description": "Chord v2 is a long-awaited update to the Qu-Bit’s original polyphonic oscillator. While gaining a tremendous reduction in HP size, the module kept all the beloved functionality of its predecessor and even attained a few new tricks up its sleeve.",
@@ -41,7 +43,7 @@ it('should display only modules with instock: true', async done => {
             "id": "zadar",
             "brand": "XAOC Devices",
             "moduleName": "Zadar Quad Envelope",
-            "image": "../assets/zadar.jpg",
+            "image": "zadar.jpg",
             "category": "Envelope",
             "size": 10,
             "description": "The XAOC Zadar is a widely configurable function generator which redefines the very notion of the modulation envelope. It consists of four independent channels which can be assigned functions made of up to 1000 segments and which can be warped, reversed, and stretched using the simple and intuitive menu system or via each channel’s assignable CV input. Each channel can output functions ranging in time from 0.08 milliseconds to 30 minutes and supports looping and chaining of waveforms, allowing for use as a complex FM source and LFO as well! With all this functionality and more in a mere 10hp, the Zadar is sure to breathe new life into any setup.",
@@ -52,7 +54,7 @@ it('should display only modules with instock: true', async done => {
             "id": "quad-vca",
             "brand": "Intellijel",
             "moduleName": "Quad VCA",
-            "image": "../assets/quad-vca.jpg",
+            "image": "quad-vca.jpg",
             "category": "VCA",
             "size": 12,
             "description": "Intellijel’s brand new Quad VCA can be used as a four individual VCAs, or as a four-channel mixer. The module offers users the ability to route each individual channel to a separate destination in their system, or use the Mix out for a sum of all four signals. The module features a continually adjustable linear to exponential response curve, and a Boost switch that adds up to 6dB of gain to each channel. ",
@@ -74,7 +76,7 @@ it('should retrieve modules in order from cheapest to most expensive', async don
             "id": "quad-vca",
             "brand": "Intellijel",
             "moduleName": "Quad VCA",
-            "image": "../assets/quad-vca.jpg",
+            "image": "quad-vca.jpg",
             "category": "VCA",
             "size": 12,
             "description": "Intellijel’s brand new Quad VCA can be used as a four individual VCAs, or as a four-channel mixer. The module offers users the ability to route each individual channel to a separate destination in their system, or use the Mix out for a sum of all four signals. The module features a continually adjustable linear to exponential response curve, and a Boost switch that adds up to 6dB of gain to each channel. ",
@@ -85,7 +87,7 @@ it('should retrieve modules in order from cheapest to most expensive', async don
             "id": "ripples",
             "brand": "Mutable Instruments",
             "moduleName": "Ripples V2 Liquid Filter",
-            "image": "../assets/ripples.jpg",
+            "image": "ripples.jpg",
             "category": "Filter",
             "size": 8,
             "description": "Ripples from Mutable Instruments is an analog multi-mode filter that sounds huge and features two cutoff slopes. Ripples offers two inputs, one transparent and clean, and one with a drive control that can saturate and distort incoming signals. It even adds enough gain to bring line-level signals into your modular. Ripples includes a high pass, low pass, and band pass filter outputs. The band pass and low pass outputs are switchable between two and four-pole operation, giving two flavors of cutoff slope. The resonance setting sets the slope of the high pass filter.",
@@ -96,7 +98,7 @@ it('should retrieve modules in order from cheapest to most expensive', async don
             "id": "ikarie",
             "brand": "BASTL",
             "moduleName": "Ikarie Stereo Filter",
-            "image": "../assets/ikarie.jpg",
+            "image": "ikarie.jpg",
             "category": "Filter",
             "size": 8,
             "description": "The next collaboration between Bastl and Casper Electronics has landed, and the Ikarie Stereo Filter is ready for spacey resonances and interstellar spatial modulation. Rather than offering discrete outputs for lowpass and highpass like most state-variable filter designs, Ikarie will continuously morph between the two, and depending on how it is patched, an array of resonant bandpass or stereo notch filtering will be heard.",
@@ -107,7 +109,7 @@ it('should retrieve modules in order from cheapest to most expensive', async don
             "id": "maths",
             "brand": "Make Noise",
             "moduleName": "Maths",
-            "image": "../assets/maths.jpg",
+            "image": "maths.jpg",
             "category": "Envelope",
             "size": 20,
             "description": "Make Noise Maths is a Control Voltage Generator and Processor indebted to the classic designs of Don Buchla in his Model 257 and Model 281 function generators. The design of Buchla's envelopes allowed them to easily switch between cycling and triggered envelopes, providing LFOs as well, and at high enough speeds, Oscillators. This design was then appropriated for the Serge Dual Universal Slope Generator, which is an apt name as it can basically create any kind of rising and falling voltage. Maths continues in this great tradition of versatile and function-packed modules.",
@@ -118,7 +120,7 @@ it('should retrieve modules in order from cheapest to most expensive', async don
             "id": "chord",
             "brand": "Qu-Bit",
             "moduleName": "Chord V2",
-            "image": "../assets/chord.jpg",
+            "image": "chord.jpg",
             "category": "Oscillator",
             "size": 14,
             "description": "Chord v2 is a long-awaited update to the Qu-Bit’s original polyphonic oscillator. While gaining a tremendous reduction in HP size, the module kept all the beloved functionality of its predecessor and even attained a few new tricks up its sleeve.",
@@ -129,7 +131,7 @@ it('should retrieve modules in order from cheapest to most expensive', async don
             "id": "zadar",
             "brand": "XAOC Devices",
             "moduleName": "Zadar Quad Envelope",
-            "image": "../assets/zadar.jpg",
+            "image": "zadar.jpg",
             "category": "Envelope",
             "size": 10,
             "description": "The XAOC Zadar is a widely configurable function generator which redefines the very notion of the modulation envelope. It consists of four independent channels which can be assigned functions made of up to 1000 segments and which can be warped, reversed, and stretched using the simple and intuitive menu system or via each channel’s assignable CV input. Each channel can output functions ranging in time from 0.08 milliseconds to 30 minutes and supports looping and chaining of waveforms, allowing for use as a complex FM source and LFO as well! With all this functionality and more in a mere 10hp, the Zadar is sure to breathe new life into any setup.",
